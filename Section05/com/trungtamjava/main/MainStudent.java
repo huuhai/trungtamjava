e89284fe6d5942b7d133f7d8633324f2936945e3
huuhai <huuhai.5.97@gmail.com>
2022-02-28 02:21:14 +0700

    Section05

---
+++ Section05/com/trungtamjava/main/MainStudent.java
@@ -0,0 +1,15 @@
+package com.trungtamjava.main;
+
+import com.trungtamjava.model.Student;
+
+public class MainStudent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Student student = new Student();
+		student.input();
+		student.info();
+	}
+
+}
